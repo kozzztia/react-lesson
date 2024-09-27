4a679c959ee333c867dec0ed3a3f602a6e4800d1
kozzztia <kozzztia@gmail.com>
2024-09-27 18:26:27 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import Home from './components/Home';
+// import About from './components/About';
+
+function App() {
+  return (
+    <div>
+      <div>react_lesson</div>
+    </div>
+  );
+}
+
+export default App;
