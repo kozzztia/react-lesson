420d6a026e7b830c6fc0f26c5984991a743a1cd1
kozzztia <kozzztia@gmail.com>
2024-09-28 10:42:16 +0300

    finish first homerork react

--- src/components/blog/Blog.js
+++ src/components/blog/Blog.js
@@ -2,9 +2,18 @@ import React from 'react';
 import style from './style.module.scss';
 
 const Blog = () => {
+  const blogs = [{ id: 1, title: "blog one", link: "#" }, { id: 2, title: "blog two", link: "#" }, { id: 3, title: "blog three", link: "#" }, { id: 4, title: "blog four", link: "#" }];
+
   return (
     <div className={style.blog}>
-        Blog
+      <h4>Blog</h4>
+      {
+        blogs.map(blog => 
+        <div key={blog.id} className={style.blogItem}>
+          <p>{blog.title}</p>
+          <a href={blog.link}>go to</a>
+      </div>)
+      }
     </div>
   )
 }
--- src/components/news/News.js
+++ src/components/news/News.js
@@ -2,9 +2,17 @@ import React from 'react';
 import style from "./style.module.scss";
 
 const News = () => {
+  const news = [{ id: 1, title: "news one", link: "#" }, { id: 2, title: "news two", link: "#" }, { id: 3, title: "news three", link: "#" }];
   return (
     <div className={style.news}>
-        News
+        <h4>News</h4>
+        {
+          news.map(news => 
+          <div key={news.id} className={style.newsItem}>
+            <p>{news.title}</p>
+            <a href={news.link}>learn more</a>
+        </div>)
+        }
     </div>
   )
 }
