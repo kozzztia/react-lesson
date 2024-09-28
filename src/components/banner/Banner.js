941206aab04a796d75ce6f5ae58346a9b7252327
kozzztia <kozzztia@gmail.com>
2024-09-28 10:48:24 +0300

    finish fix lorem in bunner and some flex in blog

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -4,7 +4,9 @@ import style from "./style.module.scss";
 const Banner = () => {
   return (
     <section className={style.banner}>
-        Banner
+        <h4>Banner</h4>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione eligendi dolore veniam fugiat cum esse laborum exercitationem accusantium consequuntur, excepturi doloribus aut autem? Quidem, assumenda?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod accusamus eligendi explicabo impedit doloremque molestiae similique reiciendis dolorum ducimus omnis alias voluptates iure cumque dolore reprehenderit saepe perspiciatis repellat aperiam, ut pariatur? Quisquam nostrum maxime quod beatae ut! Id, eaque?</p>
     </section>
   )
 }
